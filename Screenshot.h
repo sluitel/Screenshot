118ab560270d8fb4387e669e17b2fe5198e30c49
Subash Luitel <luitel@me.com>
2015-09-18 10:26:31 -0500

    added files

---
+++ Screenshot.h
@@ -0,0 +1,16 @@
+//
+//  Screenshot.h
+//  Selfie
+//
+//  Created by Subash Luitel on 2/12/15.
+//  Copyright (c) 2015 Thinkboks LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Screenshot : NSObject
+
++(UIImage *)screenshotPresentContext;
+
+@end
